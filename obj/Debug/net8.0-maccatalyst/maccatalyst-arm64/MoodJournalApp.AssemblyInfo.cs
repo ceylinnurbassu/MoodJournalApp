eceff16334c62903a77544b601d843f9bf474ecc
ceylinnurbassu <ceylinnurbassu@hotmail.com>
2025-11-07 15:53:06 +0300

    LAST CHANGES

--- obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MoodJournalApp.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/MoodJournalApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MoodJournalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c1934ad36e53d67f3c7c7b24c65d5c9838e10055")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+547fe4bb0a6de985a4025f5bfc3c3a10123a3758")]
 [assembly: System.Reflection.AssemblyProductAttribute("MoodJournalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MoodJournalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/nativelibraries/aot-output/arm64/System.Private.CoreLib.dll.s
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-arm64/nativelibraries/aot-output/arm64/System.Private.CoreLib.dll.s
@@ -50474,7 +50474,7 @@ _mono_aot_file_info:
 	.long 0,0,0,296,296,296,296,296
 	.long 296,0,0,0,0,0,0,24
 	.long 24,60,24,0,24
-	.byte 23,220,2,215,211,153,112,102,93,0,194,180,246,249,92,218
+	.byte 135,147,176,145,186,169,1,229,202,201,117,123,219,115,229,64
 	.globl _mono_aot_module_System_Private_CoreLib_info
 	.align 3
 _mono_aot_module_System_Private_CoreLib_info:
